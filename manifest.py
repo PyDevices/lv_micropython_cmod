57a5c75dd2595f3ca2e3b039841fea611dff7125
Brad Barnett <bbarnett@gmail.com>
2026-06-17 01:41:17 -0500

    Remove bundled lv_utils and lv_timer from the manifest.
    These Python event-loop helpers are not needed for the CircuitPython build and were causing packaging conflicts.

--- manifest.py
+++ manifest.py
@@ -1,2 +1,2 @@
-module("lv_utils.py", base_path="./lib", opt=3)
-module("lv_timer.py", base_path="./lib", opt=3)
+# module("lv_utils.py", base_path="./lib", opt=3)
+# module("lv_timer.py", base_path="./lib", opt=3)
